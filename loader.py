23b1a36c0a6d81fd20fe54ac607126deb85e2f8d
Akira Tanaka <akira.tanaka.1897@gmail.com>
2015-12-08 01:22:27 +0900

    マルチアカウントおよび設定読み込み対応

--- loader.py
+++ loader.py
@@ -1,33 +1,28 @@
 import re
 import os
+from default_settings import DEFAULT_SETTINGS
 
-def LoadUserData():
-	path_to_userdata = os.environ.get('HOME') + '/.popotter.user'
-	if os.path.exists(path_to_userdata):
-		userdata = open(path_to_userdata)
-
-		user_data = []
-
-		i = 0
-
-		for line in user_data:
-			rawdata = re.sub('\n', '', line)
-			current_data = re.split(',',line)
-			user_data.append(current_data)
-
-		return user_data
-	else:
-		return False
+SETTING_ITEMS = ('CONT_PRE', 'CONT_POST')
+def LoadUsersData():
+  path_to_userdata = os.environ.get('HOME') + '/.popotter.users'
+  if os.path.isfile(path_to_userdata):
+    raw_user_data = open(path_to_userdata)
+    users_data = []
+    for line in raw_user_data:
+      line = line[:-1]
+      users_data.append(line.split(','))
+    return users_data
+  return None
 
 def LoadSettings():
-  path_to_config = os.environ.get('HOME') + '/.popotter.config'
-  DEFAULT_SETTINGS = {'default-id': 0, 'time-diff': 9}
   settings = DEFAULT_SETTINGS
-  if os.path.exists(path_to_config):
+  path_to_config = os.environ.get('HOME') + '/.popotter.config'
+  if os.path.isfile(path_to_config):
     config_file = open(path_to_config)
     for line in config_file:
-      line = re.sub('\n', '', line)
-      for current_item in DEFAULT_SETTINGS:
-        if re.search(current_item + '=', line):
-          settings[current_item] = re.sub(current_item + '=', '', line)
+      line = line[:-1]
+      itemname = line.split('=')[0]
+      value = line.split('=')[1]
+      settings[itemname] = value
   return settings
+
